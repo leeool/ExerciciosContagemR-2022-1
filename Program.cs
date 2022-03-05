cf290477aa70d2be60db11d1408e853c9e7c15f3
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-03-04 21:56:31 -0300

    finaliza

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("PREPARAR...");
+Console.ReadKey();
+Console.WriteLine("APONTAR...");
+Console.ReadKey();
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("FOGO!");
+Console.ResetColor();
